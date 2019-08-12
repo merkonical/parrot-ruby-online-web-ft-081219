029bfb404637136cca7ee6368fa4c20f01466de0
Maverick Adolfo <merkonical@gmail.com>
2019-08-12 17:04:45 +0000

    Automatically backed up by Learn

--- parrot.rb
+++ parrot.rb
@@ -1,7 +1,6 @@
 # Create method `parrot` that outputs a given phrase and
 # returns the phrase
 def parrot)
-    puts "Squawk!"
-    phrase = parrot
-  end 
+  puts "Squawk!"
+  phrase = "Squawk!"
 end 
